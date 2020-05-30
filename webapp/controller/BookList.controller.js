2a4ba85ffa4955a0c00bdedaaa54577fd064c26c
teopridon <tpridon@gmail.com>
2020-05-30 19:33:06 +0300

    init checkoutsList SAPUI5

---
+++ webapp/controller/BookList.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+ ], function (Controller, MessageToast, Fragment) {
+    "use strict";
+    return Controller.extend("org.ubb.books.controller.BookList", {
+
+    });
+ });
